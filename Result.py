b505fb14bee27928734cfa7bd8ad8d628ddc1c60
Nguyen Van Huy <huydsai02@gmail.com>
2021-10-16 22:36:13 +0700

    add function

--- Result.py
+++ Result.py
@@ -6,8 +6,8 @@ from p import *
 
 # Gọi mê cung và thông tin mê cung
 maze = CreateMaze()
-xs, ys = maze.start_point
-xf, yf = maze.end_point
+xs, ys = maze.get_start_point()
+xf, yf = maze.get_end_point()
 road = maze.path
 list_maze = maze.get_list_maze()
 n = maze.n
