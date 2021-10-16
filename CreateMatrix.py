11e879e8b299667059d4d6eb1487231f8b842a92
Nguyen Van Huy <huydsai02@gmail.com>
2021-10-16 19:37:09 +0700

    Create starting point and ending point at any position

--- CreateMatrix.py
+++ CreateMatrix.py
@@ -3,12 +3,17 @@ from random import randint
 
 class Maze(object):
   def __init__(self):
-    
     # self.n là kích thước của mê cung
     self.n = 10
     self.matrix = [[0 if (i * j) % 2 == 0 else 1 for i in range(self.n)] for j in range(self.n)]
-    self.start_point = (0, randint(0, self.n - 1))
-    self.end_point = (self.n-1, randint(0, self.n - 1))
+    self.start_point = (randint(0, self.n - 1), randint(0, self.n - 1))
+    self.end_point = (randint(0, self.n - 1), randint(0, self.n - 1))
+
+  def get_start_point(self):
+    return self.start_point
+  
+  def get_end_point(self):
+    return self.end_point
   
   def RandomBrick(self):
     # Tạo ra các bức tường ngẫu nhiên
@@ -30,7 +35,7 @@ class Maze(object):
     l = self.RandomBrick()
     r = self.CreateRoad()
     n = self.n
-    # print(l)
+
     for a,b in l:
       self.matrix[a][b] = 1
 
@@ -38,42 +43,30 @@ class Maze(object):
       for j in range(n):
         if (i,j) in r:
           self.matrix[i][j] = 0
-          if i == n-1 and (i-1,j) not in r:
-            self.matrix[i-1][j] = 1
-      
+
     return self.matrix
-
-
-
+      
 
   def CreateRoad(self):
+
+    # Tạo ra 1 con đường để đi từ điểm đầu đến điểm cuối
     n = self.n
     path = []
+    
+    s = self.start_point
+    e = self.end_point
 
-    start_point = self.start_point
-    end_point = self.end_point
-
-    # Tạo ra những điểm phải đi qua
-    if n % 2 == 0:
-      cbd = n-2
-    else:
-      cbd = n-1
-    even = [i for i in range(2,cbd,2)]
-    point_must_go = [start_point]
-    for _ in even:
-      r = even[randint(0,len(even) - 1)]
-      point_must_go.append((_,r))
-    point_must_go.append(end_point)
-    # print(point_must_go)
-    for i in range(len(point_must_go) - 1):
-      s = point_must_go[i]
-      e = point_must_go[i+1]
-      for j in range(s[0], e[0] + 1):
-        path.append((j, s[1]))
-      for k in range(min(s[1],e[1]), max(s[1],e[1]) + 1):
-        path.append((e[0], k))
+    for i in range( min((s[0], e[0])) , max((s[0],e[0])) + 1):
+      path.append((i, s[1]))
+    for j in range( min((s[1], e[1])) , max((s[1],e[1])) + 1):
+      path.append((e[0], j))
+    
     self.path = path
     return self.path
+
+
+
+
 
     
 
