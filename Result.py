7448d3759b5b89778657437e6c8c423f0955d9b7
Nguyen Van Huy <huydsai02@gmail.com>
2021-10-18 22:43:10 +0700

    update FPS

--- Result.py
+++ Result.py
@@ -6,7 +6,7 @@ from p import *
 
 # size lấy vào kích cỡ mê cung với tham số thứ nhất là số ô ngang mê cung, tham số thứ 2 là số ô dọc mê cung
 # Bây giờ mê cung sẽ luôn có path và size luôn là 2 số lẻ
-width, height = (131, 65)
+width, height = (31, 15)
 maze = Maze(size = (width, height), s = (1, height - 2), e = (width - 2, 1))
 
 # Thông tin mê cung
@@ -32,7 +32,7 @@ color_brick = (102, 38, 60)
 
 ##### Thông số cửa sổ
 pygame.init()
-square = 10
+square = 20
 SIZE = (square*size[0], square*size[1] + square)
 DISPLAYSURF = pygame.display.set_mode((SIZE[0], SIZE[1]))
 pygame.display.set_caption('Maze')
@@ -90,7 +90,12 @@ def LaNgoCut(x, y, l = list_maze):
       return True
   return False
 
+#Set FPS
+FPS = 60
+fpsClock = pygame.time.Clock()
+
 while True:
+  fpsClock.tick(FPS)
   DISPLAYSURF.fill(BACKGROUND_COLOR)
   for i in range(size[0]):
     for j in range(size[1]):
