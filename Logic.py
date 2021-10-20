a43f47ca7ddd9861670781db29b8f9ed476225f1
Nguyen Van Huy <huydsai02@gmail.com>
2021-10-20 09:20:08 +0700

    Complete my logic

--- Logic.py
+++ Logic.py
@@ -7,58 +7,29 @@ class Cell(object):
   def __init__(self, coor, maze, main_road = False):
     self.x = coor[0]
     self.y = coor[1]
-    self.maze = maze
-    self.info_road = self.RoadGoThrough()
+    self.maze = maze    
     self.main_road = main_road
-    self.choice = len(self.info_road)
-    
-  
+    if main_road == False:
+      self.times = 0
+    else:
+      self.times = 1
+
+  def TimesGoPoint(self):
+    maze = self.maze
+    x, y = self.get_pos()
+    if maze.get_list_maze()[x][y] != 1:
+      valid_dimension = len(FindValidDimension((x,y) , maze.get_list_maze()))
+      times = self.times
+      if times < valid_dimension:
+        times += 1
+      self.times = times
+    return self.times
+      
   def get_pos(self):
     return (self.x, self.y)
 
-  def get_info_road(self):
-    return self.info_road
-
-  def RoadGoThrough(self):
-    maze = self.maze
-    self.info_road = {}
-    self.valid_dimension = FindValidDimension(self.get_pos() , maze.get_list_maze())
-    for d in self.valid_dimension:
-      road = FindPath(self.maze, start = self.get_pos(), c = d)
-      self.info_road[d] = Road(road, maze)
-    return self.info_road
-
-class Road(object):
-  def __init__(self, route, maze):
-    self.route = route
-    self.maze = maze
-    self.AnalysisRoad()
-
-  def AnalysisRoad(self):
-    route = self.route
-    list_point = self.maze.get_list_point()
-    self.start = route[0]
-    self.end = route[-1]
-    self.point = list_point[self.start[0]][self.start[1]]
-    self.length = len(route) - 1
-
-  def get_start(self):
-    return self.start
-
-  def get_end(self):
-    return self.end
-
-  def get_route(self):
-    return self.route
-  
-  def get_point(self):
-    return self.point
-  
-  def get_length(self):
-    return self.length
-
-
-
+  def get_times(self):
+    return self.times
 
 # Hàm bên ngoài phụ trợ
 def FindValidDimension(coor, list_maze, p = None):
@@ -82,7 +53,7 @@ def FindValidDimension(coor, list_maze, p = None):
   
   return res
 
-def FindPath(maze,start = (1,1) ,end = None, c = None, path = None): #Lỗi logic hàm này
+def FindPath(maze,start = (1,1) ,end = None, c = None, points = None): #Lỗi logic hàm này
   xs, ys = start
   list_maze = maze.get_list_maze()
   d = {}
@@ -91,7 +62,7 @@ def FindPath(maze,start = (1,1) ,end = None, c = None, path = None): #Lỗi logi
   while True:
     if len(d[coors[-1]]) == 0:
       coors.pop()
-      xs, ys = coors.pop()
+      xs, ys = coors[-1]
     else:
       dim = d[(xs,ys)]
       cpath = dim.pop()
@@ -100,79 +71,70 @@ def FindPath(maze,start = (1,1) ,end = None, c = None, path = None): #Lỗi logi
       d[(xs,ys)] = d.get((xs,ys), FindValidDimension((xs,ys), list_maze, p = cpath))
       if (xs, ys) not in coors:
         coors.append((xs, ys))
-    if end == None and path == None:
-      if (xs, ys) in StopCell(maze):
-        return coors
-    elif end == None:
-      if (xs, ys) in path:
+    # print(coors[-1], (xs,ys))
+    if end == None and points != None:
+      if (xs, ys) in points:
         return coors
     else:
       if (xs, ys) == end:
         return coors
 
 
-def StopCell(maze):
+def ConsiderCell(maze, path = []):
   size = maze.get_size()
-  list_maze = maze.get_list_maze()
   list_point = maze.get_list_point()
   res = []
   for x in range(size[0]):
     for y in range(size[1]):
-      if list_maze[x][y] != 1:
-        n = len(FindValidDimension((x,y), list_maze))
-        a = n == 3 or n == 4 or n == 1
-        b = list_point[x][y] != 0 
-        c = list_maze[x][y] == 2 or list_maze[x][y] == 3
-        if a or b or c:
-          res.append((x,y))
+      if list_point[x][y] != 0 and (x,y) not in path:
+        res.append((x, y))
   return res
 
 def Optimize_solution(start, end, maze):
-  xs, ys = start
-  xf, yf = end
   list_maze = maze.get_list_maze()
-  list_stop_cell = StopCell(maze)
   list_point = maze.get_list_point()
   size = maze.get_size()
   main_path = FindPath(maze = maze, start = start, end = end)
+  consider_cell = ConsiderCell(maze, path = main_path)
+  max = 0
   l = [[0 for i in range(size[1])] for j in range(size[0])]
-  not_main_path = []
   for i in range(size[0]):
     for j in range(size[1]):
-      if (i,j) in list_stop_cell:
-        if (i,j) in main_path:
-          l[i][j] = Cell((i,j), maze, main_road = True)
-        else:
-          not_main_path.append((i,j))
-          l[i][j] = Cell((i,j), maze, main_road = False)
+      if (i, j) not in main_path:
+        l[i][j] = Cell((i,j), maze, main_road = False)
+      else:
+        l[i][j] = Cell((i,j), maze, main_road = True)  
   all_path = []
-  for sl in range(0, len(not_main_path) + 1):
-    for subset in itertools.combinations(not_main_path, sl):
+  for sl in range(0, len(consider_cell) + 1):
+    for subset in itertools.combinations(consider_cell, sl):
       total_path = main_path[:]
       lss = list(subset)
       for coordinate in lss:
-        extra_path = FindPath(maze = maze, start = coordinate, path = main_path)
-        for eer in extra_path:
-          if eer not in main_path:
-            total_path.append(eer)
+        extra_path = FindPath(maze = maze, start = coordinate, points = main_path)
+        for coo in extra_path:
+          l[coo[0]][coo[1]].TimesGoPoint()
+          if coo not in total_path:
+            total_path.append(coo)
       score = 0
-      for eer1 in total_path:
-        if list_point[eer1[0]][eer1[1]] != 0:
-          score += list_point[eer1[0]][eer1[1]]
-      all_path.append((score, len(total_path)))
+      length = 0
+      for concoor in total_path:
+        score += list_point[concoor[0]][concoor[1]]
+        length += l[concoor[0]][concoor[1]].get_times()
+      all_path.append((score, length))
+      if score / length >= max:
+        op = (score, total_path)
+  return op
 
 if __name__ == '__main__':
-  while True:
-    s = (random.choice(range(1,19,2)),random.choice(range(1,19,2)))
-    e = (random.choice(range(1,19,2)),random.choice(range(1,19,2)))
-    maze = Maze(size = (21,21), s= s , e = e)
-    coordinate = (random.choice(range(1,19,2)),random.choice(range(1,19,2)))
-    main_path = FindPath(maze = maze, end = e, start=s)
-    extra_path = FindPath(maze = maze, start = coordinate, path = main_path)
-    print(coordinate)
-    print(extra_path)
+  si = 15
+  s = (random.choice(range(1,si - 2,2)),random.choice(range(1,si-2,2)))
+  e = (random.choice(range(1,si - 2,2)),random.choice(range(1,si - 2,2)))
+  maze = Maze(size = (si,si), s= s , e = e)
+  coordinate = (random.choice(range(1,si - 2,2)),random.choice(range(1,si - 2,2)))
+  main_path = FindPath(maze = maze, end = e, start=s)
+  extra_path = FindPath(maze = maze, start = coordinate, points = main_path)
+  print(Optimize_solution(s, e, maze))
 
-  # print(Optimize_solution(s, e, maze))
 
   # list_maze = maze.get_list_maze()
   # list_point = maze.get_list_point()
---
+++ Result1.py
@@ -0,0 +1,145 @@
+import pygame, sys
+from pygame.locals import *
+from CreateMatrix import *
+from random import randint
+from Logic import *
+
+# size lấy vào kích cỡ mê cung với tham số thứ nhất là số ô ngang mê cung, tham số thứ 2 là số ô dọc mê cung
+# Bây giờ mê cung sẽ luôn có path và size luôn là 2 số lẻ
+width, height = (15, 15)
+maze = Maze(size = (width, height))
+
+# Thông tin mê cung
+xs, ys = maze.get_start_point()
+xf, yf = maze.get_end_point()
+list_maze = maze.get_list_maze()
+size = maze.get_size()
+
+# Tính toán. Nếu muốn tìm đường đi thì calculate = True không thì False
+calculate = True
+
+if calculate == True:
+  score, optimal_path = Optimize_solution((xs, ys), (xf, yf), maze)
+  len_of_best = len(optimal_path)
+  highest_score = score / len_of_best
+  point_of_best = score
+
+###### Màu
+color_start = (255, 199, 0)
+color_end = (115, 201, 62)
+BACKGROUND_COLOR = (55, 155, 255)
+color_road = (252, 251, 250)
+color_brick = (102, 38, 60)
+
+##### Thông số cửa sổ
+pygame.init()
+square = 20
+SIZE = (square*size[0], square*size[1] + square)
+DISPLAYSURF = pygame.display.set_mode((SIZE[0], SIZE[1]))
+pygame.display.set_caption('Maze')
+
+#Upload image
+# img = pygame.image.load("./img/brick.png")# replace by ur path
+decrease = 0
+# brick = pygame.transform.scale(img, (square - 2*decrease, square - 2*decrease))
+
+# Add score
+font = pygame.font.Font('freesansbold.ttf', 10)
+def write_score(maze, x, y):
+  info = maze.get_list_point()[x][y]
+  if info != 0:
+    s = str(info)
+  else:
+    s = ""
+  text = font.render(s, True, (255,0,0))
+  return text
+
+def ShowInfo(Info, size=30):
+  fnt = pygame.font.Font('freesansbold.ttf', size)
+  text = fnt.render(str(Info), True, (255, 0, 0))
+  return text
+
+def NextPosition(x, y, step, l = list_maze):
+  nx = x + step[0]
+  ny = y + step[1]
+
+  while not CanTurn(nx, ny) and not LaNgoCut(nx, ny):
+    nx += step[0]
+    ny += step[1]
+
+  return (nx,ny)
+
+def CanTurn(x, y, l = list_maze):
+  check_direction = [[(1,0),(0,1)], [(-1,0),(0,1)], [(1,0),(0,-1)], [(-1,0),(0,-1)]]
+  for pair in check_direction:
+    count = 0
+    for d in pair:
+      if l[x + d[0]][y + d[1]] != 1:
+        count += 1
+    if count == 2:
+      return True
+  return False
+
+def LaNgoCut(x, y, l = list_maze):
+  check_direction = [[(1,0),(0,1),(0,-1)], [(-1,0),(0,1),(0,-1)], [(1,0),(-1,0),(0,-1)], [(-1,0),(1,0),(0,1)]]
+  for pair in check_direction:
+    count = 0
+    for d in pair:
+      if l[x + d[0]][y + d[1]] == 1:
+        count += 1
+    if count == 3:
+      return True
+  return False
+
+#Set FPS
+FPS = 60
+fpsClock = pygame.time.Clock()
+
+while True:
+  fpsClock.tick(FPS)
+  DISPLAYSURF.fill(BACKGROUND_COLOR)
+  for i in range(size[0]):
+    for j in range(size[1]):
+      if list_maze[i][j] == 1:
+        # DISPLAYSURF.blit(brick, (i * square + decrease, j * square + decrease))
+        pygame.draw.rect(DISPLAYSURF, color_brick, (i * square + decrease, j * square + decrease, square - 2 * decrease, square - 2 * decrease))
+      if list_maze[i][j] != 1:
+        pygame.draw.rect(DISPLAYSURF, color_road, (i * square + decrease, j * square + decrease, square - 2 * decrease, square - 2 * decrease))
+
+  pygame.draw.rect(DISPLAYSURF, color_start, (xs * square, ys * square, square, square))
+  pygame.draw.rect(DISPLAYSURF, color_end, (xf * square, yf * square, square, square))
+
+  if calculate == True:
+    for i in range(size[0]):
+      for j in range(size[1]):
+        if list_maze[i][j] == 0:
+          DISPLAYSURF.blit(write_score(maze,i,j), (i * square + decrease, j * square + decrease))
+
+    for x,y in optimal_path:
+      pygame.draw.circle(DISPLAYSURF, (0, 0, 255), (x*square + square/2, y*square + square/2), square/4, square//4)
+    DISPLAYSURF.blit(ShowInfo(f'The highest score is {round(highest_score,2)} with {len_of_best} steps and {int(point_of_best)} points', size=15), (square/2, SIZE[1] - square))
+
+  for event in pygame.event.get():
+      if event.type == QUIT or (xs,ys) == (xf,yf):
+          pygame.quit()
+          sys.exit()
+      if event.type == pygame.KEYDOWN:
+        if event.key in [K_s, K_DOWN]:
+          if list_maze[xs][ys+1] != 1:
+            xs, ys = NextPosition(xs,ys,(0,1))
+
+        if event.key in [K_w,K_UP]:
+          if list_maze[xs][ys-1] != 1:
+            xs, ys = NextPosition(xs,ys,(0,-1))
+
+        if event.key in [K_a, K_LEFT]:
+          if list_maze[xs-1][ys] != 1:
+            xs, ys = NextPosition(xs,ys,(-1,0))
+
+        if event.key in [K_d, K_RIGHT]:
+          if list_maze[xs+1][ys] != 1:
+            xs, ys = NextPosition(xs,ys,(1,0))
+
+  pygame.display.update()
+
+
